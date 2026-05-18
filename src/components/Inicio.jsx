c5bc314b4cd08cbac34c8cb3214a5dcfb0b51164
Fernando Hernandez <fernandohernandez@MacBook-Air-de-Fernando.local>
2026-05-17 23:20:19 -0600

    Fix imagen inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,10 +1,15 @@
 function Inicio() {
+    const baseUrl =
+    import.meta.env.MODE === "production"
+      ? "/estudio-fotografico-NatalyVazquez-Fotografia-"
+      : ""
+
   return (
     <section id="inicio" className="inicio">
       <h1>Nataly Vázquez Fotografía</h1>
       <p>Capturando momentos únicos e inolvidables.</p>
       <img 
-        src="/images/foto1.jpg" 
+        src={`${baseUrl}/images/foto1.jpg`}
         alt="Fotografía principal"
         className="imagen-principal"
       />
